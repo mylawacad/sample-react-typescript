86a54e2b276dbe7522f6d5b2626cf85c0a8d84bf
mylawacad <mylawacad@gmail.com>
2022-05-23 16:56:56 +0200

    - cleanup default template code - add Layout section - add Header section - add HomePage Page - add Link Component - add LinkImagePlaceholder Component - add ListingColored Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Layout from './Pages/Layout';
+import "bootstrap/dist/css/bootstrap.css"; // Import Bootstrap css
+import "@fortawesome/fontawesome-free/css/all.css"; // Import FontAwesome Icons
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Layout />
+  )
 }
 
 export default App;
+
